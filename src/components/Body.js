74370a1ae3cdc18486e278f5122579dc0832e8f5
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-06-08 16:43:05 +0300

    Weather API: Extracting the values from the Fake Weather json.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,10 @@
 import React ,{Component} from "react";
 import fakeWeatherData from "../fakeWeatherData.json";
-
 import mostlycloudy from "../img/weather-icons/mostlycloudy.svg";
-
 
 class Body extends React.Component {
     state = {
-      input: ""
+      input: " "
     };
   
     render() {
@@ -17,8 +15,10 @@ class Body extends React.Component {
       <div class="middle">
       <img src ={mostlycloudy} alt="mostltCloudy"/>
       <div class="text-under"> overcast clouds</div>
-      <div class="under-under">Temperature {Math.floor(fakeWeatherData.list[0].main.temp - 273.15)}<span>&#176;</span> to {Math.floor(fakeWeatherData.list[1].main.temp - 273.15)} C</div>
-      <div class="unders"> <b>HUmidity </b> {fakeWeatherData.list[0].main.humidity}% <b>pressure</b> {fakeWeatherData.list[0].main.pressure}</div>
+
+      <div class="under-under">Temperature. {Math.floor(fakeWeatherData.list[0].main.temp - 273.15)} <span>&#176;</span> to {Math.floor(fakeWeatherData.list[1].main.temp - 273.15)} C</div>
+
+      <div class="unders"> <b>HUmidity. </b> {fakeWeatherData.list[0].main.humidity}% <b>pressure</b> {fakeWeatherData.list[0].main.pressure}</div>
     </div>
 
 
